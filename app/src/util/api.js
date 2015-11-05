76a77a5fac814813855f4a665caaad5856c458ca
Ramon Gebben <ramon@touchtribe.nl>
2015-11-05 11:14:52 +0100

    made api endpoint generic

--- app/src/util/api.js
+++ app/src/util/api.js
@@ -2,14 +2,8 @@
 const API = {
     getAll: (type) => {
         switch (type) {
-            case 'posts':
-                return 'Not implemented yet';
-                // return fetch('http://localhost:3000/posts').then(data => {
-                //     console.log(data);
-                // });
-                break;
             case 'authors':
-                return fetch('http://localhost:3000/authors').then(data => data.json());
+                return fetch(window.location.href +':3000/authors').then(data => data.json());
                 break;
             default:
                 // Do nothing
